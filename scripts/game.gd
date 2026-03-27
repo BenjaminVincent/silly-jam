d4438d11003dd781fcfa917a00516fc0f31a1add
Ben <ben.tom.vincent@gmail.com>
2026-03-27 16:28:24 -0700

    added tilemaplayer background and foreground with some basic scrolling

---
+++ scripts/game.gd
@@ -0,0 +1,2 @@
+extends Node2D
+@onready var level: Node2D = $level
---
+++ scripts/level.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var forground: TileMapLayer = $Forground
+
+
+var scroll_speed = 20
+
+func _process(delta):
+	forground.position.x -= scroll_speed * delta
