59be7d396fc9b4b0c1c609be8da722a8ea50cdfd
Ben <ben.tom.vincent@gmail.com>
2026-03-27 17:38:22 -0700

    updated cursor

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,2 +1,12 @@
 extends Node2D
 @onready var level: Node2D = $level
+
+@onready var cursor: Sprite2D = $UI/Cursor
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
+
+func _process(_delta: float) -> void:
+	cursor.position = get_global_mouse_position()
