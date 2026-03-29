912b181aee50b8312ddbe893684c2a57af2e32ee
Ben <ben.tom.vincent@gmail.com>
2026-03-29 15:07:40 -0700

    cleaned up logs

--- scripts/game_over_panel.gd
+++ scripts/game_over_panel.gd
@@ -2,14 +2,9 @@ extends Control
 
 var scroll_speed = GlobalStatics.scroll_speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("game over")
-	
 
 
 func _on_quit_pressed() -> void:
-	print("quit game")
 	get_tree().quit()
 
 
