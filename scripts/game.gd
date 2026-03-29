f984d11a17fe30bcfb8deeeb2314868b7422ff5f
Ben <ben.tom.vincent@gmail.com>
2026-03-29 16:15:46 -0700

    disabled non full screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -26,14 +26,14 @@ func _process(_delta: float) -> void:
 
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and not event.echo and (event.keycode == KEY_ESCAPE):
-		_on_pressed()
-
-
-
-func _on_pressed() -> void:
-	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventKey and event.pressed and not event.echo and (event.keycode == KEY_ESCAPE):
+		#_on_pressed()
+#
+#
+#
+#func _on_pressed() -> void:
+	#if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	#else:
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
